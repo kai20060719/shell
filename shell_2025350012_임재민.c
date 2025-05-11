c07169802bca910934b01ce831bb9c8b1b8029ff
kai20060719 <kai20060719@gmail.com>
2025-05-11 16:32:05 +0900

    Update shell_2025350012_임재민.c
    리눅스에서 구현되도록 변환

--- shell_2025350012_임재민.c
+++ shell_2025350012_임재민.c
@@ -1,48 +1,46 @@
-
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
-#include <direct.h> 
+#include <unistd.h> 
 #define MAX_ARGS 64
 #define MAX_LINE 256
 
 void wordsep(char* line, char** args) {
-	int i = 0;
-	args[i] = strtok(line, " \t\n");
-	while (args[i] != NULL && i <= MAX_ARGS - 2) {
-		i++;
-		args[i] = strtok(NULL, " \n\t");
-	}
-	args[i] = NULL;
+        int i = 0;
+        args[i] = strtok(line, " \t\n");
+        while (args[i] != NULL && i <= MAX_ARGS - 2) {
+                i++;
+                args[i] = strtok(NULL, " \n\t");
+        }
+        args[i] = NULL;
 }
 
 int main(void) {
-	char line[MAX_LINE];
-	char* args[MAX_ARGS];
-	while (1) {
-		printf("kai2006>");
-		fflush(stdout);
+        char line[MAX_LINE];
+        char* args[MAX_ARGS];
+        while (1) {
+                printf("kai2006>");
+                fflush(stdout);
 
-		if (fgets(line, sizeof(line), stdin) == NULL) {
-			break;
-		}
-		wordsep(line, args);
+                if (fgets(line, sizeof(line), stdin) == NULL) {
+                        break;
+                }
+                wordsep(line, args);
 
-		if (args[0] == NULL) {
-			continue;
-		}
+                if (args[0] == NULL) {
+                        continue;
+                }                if (strcmp(args[0], "exit") == 0) {
+                        break;
+                }
 
-		if (strcmp(args[0], "exit") == 0) {
-			break;
-		}
+                if (strcmp(args[0], "cd") == 0) {
 
-		if (strcmp(args[0], "cd") == 0) {
-
-			if (args[1] != NULL)
-				_chdir(args[1]);
-			else
-				fprintf(stderr, "cd:missing operand\n");
-			continue;
-		}
-	}
+                        if (args[1] != NULL){
+                             if(chdir(args[1]) != 0)
+                                  perror("cd");
+                          }
+                        else
+                                fprintf(stderr, "cd:missing operand\n");
+                        continue;
+                }
+        }
 }
